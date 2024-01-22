6b0f9259fdd7cc1b3b40385c555d5597d7e07fc9
Enio Fernandes <EN10@users.noreply.github.com>
2024-01-22 21:03:22 +0000

    Update networking.sh

--- networking.sh
+++ networking.sh
@@ -11,6 +11,7 @@ make CONFIG_PREFIX=../initramfs install
 
 
 # libnss_dns files for networking
+# https://github.com/EN10/BusyBoxLinux/blob/main/lib/lib-files.md
 cd ../initramfs
 wget https://github.com/EN10/BusyBoxLinux/raw/main/lib/lib.tar.gz
 tar -xvzf lib.tar.gz
