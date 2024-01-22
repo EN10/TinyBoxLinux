47ff19d9357a7376177b97972bb16f1f50064f68
Enio Fernandes <EN10@users.noreply.github.com>
2024-01-22 20:18:55 +0000

    Update networking.sh

--- networking.sh
+++ networking.sh
@@ -13,7 +13,7 @@ make CONFIG_PREFIX=../initramfs install
 mkdir ../pkg
 cd ../pkg
 sudo apt install apt-rdepends
-apt-rdepends libc6 | awk '{ print $1 }' | sort | uniq | tr '\n' ' ' | cut -d" "  -f3- > deps.txt
+apt-rdepends libc6 | awk '{ print $1 }' | sort | uniq | tr '\n' ' ' | cut -d " " -f3- > deps.txt
 apt download $(cat deps.txt)
 
 mkdir -p ../initramfs//var/lib/dpkg/updates ../initramfs//var/lib/dpkg/info  && touch ../initramfs//var/lib/dpkg/status
