3ad1b868e6334c161a3e34f41adad25cc478f9a9
Enio Fernandes <EN10@users.noreply.github.com>
2024-01-22 20:41:28 +0000

    Update networking.sh

--- networking.sh
+++ networking.sh
@@ -15,6 +15,10 @@ cd ../pkg
 sudo apt install apt-rdepends
 apt-rdepends libc6 | awk '{ print $1 }' | sort | uniq | tr '\n' ' ' | cut -d " " -f3- > deps.txt
 apt download $(cat deps.txt)
+# alternative to apt
+cd ../initramfs
+wget https://github.com/EN10/BusyBoxLinux/raw/main/lib.tar.gz
+tar -xvzf lib.tar.gz
 
 mkdir -p ../initramfs//var/lib/dpkg/updates ../initramfs//var/lib/dpkg/info  && touch ../initramfs//var/lib/dpkg/status
 sudo dpkg --root ../initramfs/ -i * 
