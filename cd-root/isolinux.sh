897d9f6e76f2f53971ecace209aac1e6396df2de
Enio Fernandes <EN10@users.noreply.github.com>
2024-01-29 23:18:11 +0000

    Update isolinux.sh

--- cd-root/isolinux.sh
+++ cd-root/isolinux.sh
@@ -10,11 +10,9 @@ wget https://raw.githubusercontent.com/EN10/BusyBoxLinux/main/cd-root/isolinux.c
 cp ../linux/arch/x86/boot/bzImage .
 # rename as init.cpio.gz becomes init.cpi.gz on iso
 cp ../init.cpio.gz ./init.gz
+
 sudo apt install mkisofs
-
-
 mkisofs -o BusyBoxLinux.iso \
    -b isolinux/isolinux.bin -c isolinux/boot.cat \
    -no-emul-boot -boot-load-size 4 -boot-info-table \
    cd-root
-
