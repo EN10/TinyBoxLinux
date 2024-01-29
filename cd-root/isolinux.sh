5e8d7e3a9ea300211e6e566dd5cbaa2a8fa48797
Enio Fernandes <EN10@users.noreply.github.com>
2024-01-29 23:16:24 +0000

    Update and rename isolinux.md to isolinux.sh

--- cd-root/isolinux.sh
+++ cd-root/isolinux.sh
@@ -1,6 +1,5 @@
-## BusyBox Linux iso
+# BusyBox Linux iso
 
-```
 mkdir -p cd-root/isolinux
 cd cd-root/isolinux
 # https://wiki.syslinux.org/wiki/index.php?title=ISOLINUX
@@ -12,10 +11,10 @@ cp ../linux/arch/x86/boot/bzImage .
 # rename as init.cpio.gz becomes init.cpi.gz on iso
 cp ../init.cpio.gz ./init.gz
 sudo apt install mkisofs
-```
-```
+
+
 mkisofs -o BusyBoxLinux.iso \
    -b isolinux/isolinux.bin -c isolinux/boot.cat \
    -no-emul-boot -boot-load-size 4 -boot-info-table \
    cd-root
-```
+
