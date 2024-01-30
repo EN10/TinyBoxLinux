0d969631bb547844ef9ce3ebb726474aeb2b91f5
Enio Fernandes <EN10@users.noreply.github.com>
2024-01-30 22:55:44 +0000

    Create BusyBox-Static.sh

---
+++ BusyBox-Static.sh
@@ -0,0 +1,16 @@
+# BusyBox Dynamic
+$ sudo chroot . bin/sh
+chroot: failed to run command ‘bin/sh’: No such file or directory
+$ ldd bin/sh
+        linux-vdso.so.1 (0x00007fff47304000)
+        libm.so.6 => /lib/x86_64-linux-gnu/libm.so.6 (0x00007f6c8a248000)
+        libresolv.so.2 => /lib/x86_64-linux-gnu/libresolv.so.2 (0x00007f6c8a22c000)
+        libc.so.6 => /lib/x86_64-linux-gnu/libc.so.6 (0x00007f6c8a03a000)
+        /lib64/ld-linux-x86-64.so.2 (0x00007f6c8a4ae000)
+
+
+# BusyBox Static
+$ ldd bin/sh
+        not a dynamic executable
+$ sudo chroot . bin/sh
+/ # 
