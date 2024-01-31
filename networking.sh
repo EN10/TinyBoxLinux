43c9ef6271641caf156d95dc4f3ef97cd7b7ad59
Enio Fernandes <EN10@users.noreply.github.com>
2024-01-31 23:42:34 +0000

    Update networking.sh

--- networking.sh
+++ networking.sh
@@ -22,7 +22,7 @@ cd ../pkg
 sudo apt install apt-rdepends
 apt-rdepends libc6 | awk '{ print $1 }' | sort | uniq | tr '\n' ' ' | cut -d " " -f3- > deps.txt
 apt download $(cat deps.txt)
-mkdir -p ../initramfs//var/lib/dpkg/updates ../initramfs//var/lib/dpkg/info  && touch ../initramfs//var/lib/dpkg/status
+mkdir -p ../initramfs//var/lib/dpkg/updates ../initramfs//var/lib/dpkg/info && touch ../initramfs//var/lib/dpkg/status
 sudo dpkg --root ../initramfs/ -i * 
 # debug dpkg
 # sudo dpkg --root ../init/ --configure -a 
