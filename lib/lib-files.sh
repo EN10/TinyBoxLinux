7d27dee3dc40568489b5f9104bf0dc8da48a5bb8
Enio Fernandes <EN10@users.noreply.github.com>
2024-02-02 21:52:51 +0000

    Update lib-files.sh

--- lib/lib-files.sh
+++ lib/lib-files.sh
@@ -21,13 +21,10 @@ cat trace.5550 | grep openat | grep -v ENOENT | awk '{print $2}'
 "/etc/resolv.conf",
 "index.html",
 
-ls -l ./initramfs/lib/x86_64-linux-gnu
+ls -l lib/x86_64-linux-gnu | awk '{print $9}'
 
-ld-linux-x86-64.so.2  
-libc.so.6  
-libnsl.so.1 -> libnsl-2.31.so
-libnss_dns.so.2  
-libnss_files.so.2 -> libnss_files-2.31.so
-libnss_nis.so.2 -> libnss_nis-2.31.so
+ld-linux-x86-64.so.2
+libc.so.6
+libnss_dns.so.2
+libnss_files.so.2
 libresolv.so.2
-
