586bde2982ee06d93511ccd602bb9a11eafd3ce3
Enio Fernandes <EN10@users.noreply.github.com>
2024-02-02 21:31:17 +0000

    Update networking.sh

--- networking.sh
+++ networking.sh
@@ -10,26 +10,14 @@ make menuconfig
 make -j 8
 make CONFIG_PREFIX=../initramfs install
 
-# libnss_dns files for networking
-# https://github.com/EN10/BusyBoxLinux/blob/main/lib/lib-files.sh
+# glibc libraries for DNS
 # https://wiki.gentoo.org/wiki/Custom_Initramfs#DNS
+# https://github.com/EN10/BusyBoxLinux/blob/main/lib/lib-files.sh
 cd ../initramfs
 # copy DNS networking files from host
 mkdir -p lib/x86_64-linux-gnu
 cp /lib/x86_64-linux-gnu/libnss_{dns,files}.so.2 /lib/x86_64-linux-gnu/{libresolv,ld-linux-x86-64}.so.2 /lib/x86_64-linux-gnu/libc.so.6 lib/x86_64-linux-gnu
-# OR download files from github
-wget https://github.com/EN10/BusyBoxLinux/raw/main/lib/lib.tar.gz
-tar -xvzf lib.tar.gz
-rm lib.tar.gz
-# OR use apt instead of wget lib.tar.gz
-mkdir ../pkg && cd ../pkg
-sudo apt install apt-rdepends
-apt-rdepends libc6 | awk '{ print $1 }' | sort | uniq | tr '\n' ' ' | cut -d " " -f3- > deps.txt
-apt download $(cat deps.txt)
-mkdir -p ../initramfs//var/lib/dpkg/updates ../initramfs//var/lib/dpkg/info && touch ../initramfs//var/lib/dpkg/status
-sudo dpkg --root ../initramfs/ -i * 
-# debug dpkg
-# sudo dpkg --root ../init/ --configure -a 
+# OR use APT: https://github.com/EN10/BusyBoxLinux/blob/main/lib/libc6-apt-dns.sh
 
 # setup /etc
 cd ../../initramfs
