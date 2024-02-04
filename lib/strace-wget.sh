f7f5c7ada94de73e18d2e33851b96fb6b37e7241
Enio Fernandes <EN10@users.noreply.github.com>
2024-02-04 00:13:13 +0000

    Update strace-wget.sh

--- lib/strace-wget.sh
+++ lib/strace-wget.sh
@@ -6,18 +6,14 @@ ps
 sudo strace -ff -p 12345 -o trace
 ping -c1 google.com
 wget example.com
-cat trace.5550 | grep openat | grep -v ENOENT | awk '{print $2}'
-# https://github.com/EN10/BusyBoxLinux/blob/main/lib/trace.5550
+cat trace.33459 | grep openat | grep -v ENOENT | awk '{print $2}'
+# https://github.com/EN10/BusyBoxLinux/blob/main/lib/trace.33459
 
-"/lib/x86_64-linux-gnu//libnss_nis.so.2",
-"/lib/x86_64-linux-gnu//libnsl.so.1",
-"/lib/x86_64-linux-gnu//libnss_files.so.2",
-"/lib/x86_64-linux-gnu//libc.so.6",
-"/lib/x86_64-linux-gnu//ld-linux-x86-64.so.2",
-"/etc/services",
 "/etc/resolv.conf",
 "/lib/x86_64-linux-gnu//libnss_dns.so.2",
 "/lib/x86_64-linux-gnu//libresolv.so.2",
+"/lib/x86_64-linux-gnu//libc.so.6",
+"/lib/x86_64-linux-gnu//ld-linux-x86-64.so.2",
 "/etc/resolv.conf",
 "index.html",
 
