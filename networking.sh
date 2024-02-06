8a0bf35387f632985d3e40faca8e60b6980f8f2c
eniompw <71254710+eniompw@users.noreply.github.com>
2024-02-06 20:11:36 +0000

    Update networking.sh

--- networking.sh
+++ networking.sh
@@ -16,8 +16,8 @@ cd ../initramfs
 lib=lib/x86_64-linux-gnu/
 mkdir -p ${lib}
 cp /${lib}{libnss_dns,libresolv,ld-linux-x86-64}.so.2 /${lib}libc.so.6 ${lib}
-# strace: https://github.com/EN10/BusyBoxLinux/blob/main/lib/strace-wget.sh
-# OR use APT: https://github.com/EN10/BusyBoxLinux/blob/main/lib/libc6-apt-dns.sh
+# strace: https://github.com/EN10/BusyBoxLinux/blob/main/lib/wget/strace-wget.sh
+# OR use APT: https://github.com/EN10/BusyBoxLinux/blob/main/lib/wget/libc6-apt-dns.sh
 
 # setup /etc
 cd ../initramfs
