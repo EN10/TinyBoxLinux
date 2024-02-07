dc8ec2f1fb92a4a69a133f50216354c49f7d1902
eniompw <71254710+eniompw@users.noreply.github.com>
2024-02-07 16:57:35 +0000

    Update strace.sh

--- lib/strace/strace.sh
+++ lib/strace/strace.sh
@@ -1,6 +1,8 @@
+# path
 which strace
 /usr/bin/strace
 
+# shared libraries
 ldd /usr/bin/strace | awk '{print $3}'
 /lib/x86_64-linux-gnu/librt.so.1
 /lib/x86_64-linux-gnu/libunwind-ptrace.so.0
@@ -17,8 +19,10 @@ xargs -a libs.txt cp -t lib/x86_64-linux-gnu
 strace
 bin/sh: strace: not found
 
+# file type
 file usr/bin/strace 
 usr/bin/strace: ELF 64-bit LSB shared object, x86-64, version 1 (SYSV), dynamically linked, interpreter /lib64/ld-linux-x86-64.so.2 ...
 
+# symbolic link
 ls -l lib64/ld-linux-x86-64.so.2 
 lib64/ld-linux-x86-64.so.2 -> /lib/x86_64-linux-gnu/ld-2.31.so
