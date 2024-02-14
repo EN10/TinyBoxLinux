9a474b431fd7f695b411863cfdd1cb47f2b00850
Enio Fernandes <EN10@users.noreply.github.com>
2024-02-14 00:45:49 +0000

    Update apt-libc6.sh

--- lib/wget/apt-libc6.sh
+++ lib/wget/apt-libc6.sh
@@ -1,12 +1,12 @@
 # use APT to install glibc libraries for DNS
-mkdir ../pkg && cd ../pkg
+mkdir pkg && cd pkg
 sudo apt install apt-rdepends
 apt-rdepends libc6 | awk '{ print $1 }' | sort | uniq | tr '\n' ' ' | cut -d " " -f3- > deps.txt
 apt download $(cat deps.txt)
-mkdir -p ../initramfs//var/lib/dpkg/updates ../initramfs//var/lib/dpkg/info && touch ../initramfs//var/lib/dpkg/status
-sudo dpkg --root ../initramfs/ -i * 
+mkdir -p ../var/lib/dpkg/updates ../var/lib/dpkg/info && touch ../var/lib/dpkg/status
+sudo dpkg --root ../ -i * 
 # debug dpkg
-# sudo dpkg --root ../init/ --configure -a 
+# sudo dpkg --root ../ --configure -a 
 
 
 # deps.txt :
